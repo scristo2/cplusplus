2fc9fc2ff978c716d019c518a1ebf6aa67cdd69b
scristo2 <77905570+scristo2@users.noreply.github.com>
2023-11-17 02:28:45 +0100

    Update textinput.cpp

--- textinput.cpp
+++ textinput.cpp
@@ -1,2 +1,15 @@
 // Crear un área de texto (wxTextCtrl) que se expande solo verticalmente
         wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxHSCROLL | wxEXPAND);
+
+
+##dtectar cambios en el input
+
+<< esto en el constructor de la clase mencionada
+Bind(wxEVT_TEXT, &FrameHome::detectTextInput, this, tested->GetId()); ##tested es la variable que le di al input
+
+void FrameHome::detectTextInput(wxCommandEvent& event) {
+
+    wxString texto = dynamic_cast<wxTextCtrl*>(wxWindow::FindWindowById(event.GetId()))->GetValue();
+
+
+}
